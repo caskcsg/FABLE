58f5f28bea58e13b0db212c4df2297ee32bbd6ff
wpwpwpyo <49509792+wpwpwpyo@users.noreply.github.com>
2026-03-03 21:44:28 +0800

    Update compute_score.sh

--- compute_score.sh
+++ compute_score.sh
@@ -3,7 +3,6 @@ model_path=../models/all-MiniLM-L6-v2
 
 post_path=$1
 python compute_score.py \
-    --pre_path $pre_path \
     --post_path $post_path \
     --model_path $model_path \
     --device $gpu
